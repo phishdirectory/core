59332a7189512a0ce03bc84b39610e7d700c4e74
Jasper Mayone <jasper.mayone@phish.directory>
2025-12-18 18:32:39 -0500

    bugfixes

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-WickedPdf.config = {
-  exe_path: Gem.bin_path("wkhtmltopdf-binary", "wkhtmltopdf"),
-  enable_local_file_access: true
-}
+WickedPdf.configure do |config|
+  config.exe_path = Gem.bin_path("wkhtmltopdf-binary", "wkhtmltopdf")
+  config.enable_local_file_access = true
+end
