71bafbd8e679f90ebcac51f462998e11a20f3276
Jasper Mayone <jasper.mayone@phish.directory>
2026-01-25 11:39:47 -0500

    more bugs with console1984

--- config/initializers/console1984.rb
+++ config/initializers/console1984.rb
@@ -1,3 +1,12 @@
 # frozen_string_literal: true
 
-Console1984.config.username_resolver = -> { ENV["USER"] || ENV["USERNAME"] || `whoami`.strip }
+# Custom resolver that checks multiple env vars
+class ConsoleUsernameResolver
+  include Console1984::Freezeable
+
+  def current
+    ENV["CONSOLE_USER"] || ENV["USER"] || ENV["USERNAME"] || `whoami`.strip
+  end
+end
+
+Console1984.config.username_resolver = ConsoleUsernameResolver.new
